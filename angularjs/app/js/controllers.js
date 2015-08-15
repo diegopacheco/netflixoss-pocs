001bede41a2b44a6ae16b29bf84dd5f444f8bfcd
diegopacheco <diego.pacheco.it@gmail.com>
2015-08-15 00:34:49 -0700

    .
    .

---
+++ angularjs/app/js/controllers.js
@@ -0,0 +1,23 @@
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+  $scope.name = "World"; 	
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1,
+     "imageUrl": "img/phones/phone0.png"
+ 	},
+    {'name': 'Motorola XOOM with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.',
+      'age': 2,
+      "imageUrl": "img/phones/phone0.png"
+ 	},
+    {'name': 'MOTOROLA XOOM',
+     'snippet': 'The Next, Next Generation tablet.',
+      'age': 3,
+      "imageUrl": "img/phones/phone0.png"
+    }
+  ];  
+  $scope.orderProp = 'age';
+});
