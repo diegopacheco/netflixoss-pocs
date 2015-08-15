ef944246eb1c15fcb279aa1b4cd63e5aebc79312
diegopacheco <diego.pacheco.it@gmail.com>
2015-08-15 01:05:36 -0700

    .
    .

--- angularjs/app/js/controllers.js
+++ angularjs/app/js/controllers.js
@@ -3,24 +3,39 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 phonecatControllers.controller('PhoneListCtrl', function ($scope) {
   $scope.name = "World"; 	
   $scope.phones = [
-    {'name': 'Nexus S',
+    {
+     'name': 'Nexus S',
      'snippet': 'Fast just got faster with Nexus S.',
      'age': 1,
-     "imageUrl": "img/phones/phone0.png"
+     "imageUrl": "img/phones/phone0.png",
+     "id": 10
  	},
-    {'name': 'Motorola XOOM with Wi-Fi',
+    {
+     'name': 'Motorola XOOM with Wi-Fi',
      'snippet': 'The Next, Next Generation tablet.',
       'age': 2,
-      "imageUrl": "img/phones/phone0.png"
+      "imageUrl": "img/phones/phone0.png",
+      "id": 11
  	},
-    {'name': 'MOTOROLA XOOM',
+    {
+     'name': 'MOTOROLA XOOM',
      'snippet': 'The Next, Next Generation tablet.',
       'age': 3,
-      "imageUrl": "img/phones/phone0.png"
+      "imageUrl": "img/phones/phone0.png",
+      "id": 12
     }
   ];  
   $scope.orderProp = 'age';
 });
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+    $scope.name = "World"; 	
+    $scope.orderProp = 'age';
+  }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
