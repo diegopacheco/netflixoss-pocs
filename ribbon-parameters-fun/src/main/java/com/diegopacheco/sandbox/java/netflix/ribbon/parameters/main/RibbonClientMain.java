b13cd687ffcc67b4b741a9fff39ab6187f0d6a50
diegopacheco <diego.pacheco.it@gmail.com>
2015-08-28 19:44:20 -0700

    .
    .

--- ribbon-parameters-fun/src/main/java/com/diegopacheco/sandbox/java/netflix/ribbon/parameters/main/RibbonClientMain.java
+++ ribbon-parameters-fun/src/main/java/com/diegopacheco/sandbox/java/netflix/ribbon/parameters/main/RibbonClientMain.java
@@ -2,6 +2,7 @@ package com.diegopacheco.sandbox.java.netflix.ribbon.parameters.main;
 
 import java.nio.charset.Charset;
 
+import com.netflix.config.ConfigurationManager;
 import com.netflix.ribbon.ClientOptions;
 import com.netflix.ribbon.Ribbon;
 import com.netflix.ribbon.RibbonResponse;
@@ -17,6 +18,10 @@ public class RibbonClientMain {
 	
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception{
+		
+		System.out.println("apiCall.ribbon.MaxAutoRetriesNextServer: " +  ConfigurationManager.getConfigInstance().getProperty("apiCall.ribbon.MaxAutoRetriesNextServer"));
+		System.out.println("apiCall.ribbon.ConnectTimeout: " +  ConfigurationManager.getConfigInstance().getProperty("apiCall.ribbon.ConnectTimeout"));
+		System.out.println("apiCall.ribbon.ReadTimeout: " +  ConfigurationManager.getConfigInstance().getProperty("apiCall.ribbon.ReadTimeout"));
 		
 		HttpResourceGroup httpRG = Ribbon.createHttpResourceGroup("apiCall",
 	            ClientOptions.create()
