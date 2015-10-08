38369ba95d3cb6ad8e4941663368b30c36e8cf45
diegopacheco <diego.pacheco.it@gmail.com>
2015-10-08 11:39:18 -0300

    added 10k inserts and gets

--- redis-jedis-cluster-playground/src/main/java/com/github/diegopacheco/sandbox/java/redis/jedis/cluster/main/SimpleClusterMain.java
+++ redis-jedis-cluster-playground/src/main/java/com/github/diegopacheco/sandbox/java/redis/jedis/cluster/main/SimpleClusterMain.java
@@ -10,17 +10,39 @@ import redis.clients.jedis.JedisCluster;
 public class SimpleClusterMain {
 
 	public static void main(String[] args) {
-		
+		test10kInsertsAndGets();
+	}
+	
+	public static void test10kInsertsAndGets(){
 		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
 		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30001));
 		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30002));
 		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30003));
+		jedisClusterNodes.add(new HostAndPort("192.169.1.115", 30001));
+		jedisClusterNodes.add(new HostAndPort("192.169.1.115", 30002));
+		jedisClusterNodes.add(new HostAndPort("192.169.1.115", 30003));
+		JedisCluster jc = new JedisCluster(jedisClusterNodes);
 		
+		for(int i=0; i<= 9999; i++){
+			jc.set("foo"+i, "bar"+i);
+			String value = jc.get("foo"+i);
+			System.out.println(value);
+		}
 		
+	}
+	
+	public static void simpleTest(){
+		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30001));
+		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30002));
+		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30003));
+
 		JedisCluster jc = new JedisCluster(jedisClusterNodes);
 		jc.set("foo", "bar");
 		
 		String value = jc.get("foo");
 		System.out.println(value);
 	}
+	
+	
 }
