da115cb6d190cf904e7519d8dc1ad10db1e045f9
diegopacheco <diego.pacheco.it@gmail.com>
2015-10-15 20:50:50 -0300

    .
    .

--- memcache-java-clients-playground/src/main/java/com/github/diegopacheco/sandbox/java/memcached/bench/XMemcachedMainBench.java
+++ memcache-java-clients-playground/src/main/java/com/github/diegopacheco/sandbox/java/memcached/bench/XMemcachedMainBench.java
@@ -9,6 +9,48 @@ import net.rubyeye.xmemcached.utils.AddrUtil;
 public class XMemcachedMainBench {
 
 	public static void main(String[] args) throws Throwable {
+		for(int i=0;i<=2;i++){
+			benchInsert(10);
+			benchGet(10);
+			
+			benchInsert(100);
+			benchGet(100);
+			
+			benchInsert(1000);
+			benchGet(1000);
+			
+			benchInsert(10000);
+			benchGet(10000);
+			
+			benchInsert(100000);
+			benchGet(100000);
+		}
+	}
+	
+	private static void benchInsert(int n) throws Throwable {
+		MemcachedClient cluster = createCluster();
+		
+		double init = System.currentTimeMillis();
+		for(int i=0;i<=(n-1);i++){
+			cluster.add("key"+i, 0, "value"+i);
+		}
+		double end = System.currentTimeMillis();
+		printBench("Insert " + n + " IDS ",init,end);
+	}
+	
+	private static void benchGet(int n) throws Throwable {
+		MemcachedClient cluster = createCluster();
+		
+		double init = System.currentTimeMillis();
+		for(int i=0;i<=(n-1);i++){
+			cluster.get("key"+i, 0);
+		}
+		double end = System.currentTimeMillis();
+		printBench("Get " + n + " IDS ",init,end);
+	}
+	
+	@SuppressWarnings("unused")
+	private static void simpleCall() throws Throwable{
 		 XMemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses("127.0.0.1:11211"));
 	     MemcachedClient client = builder.build();
 	     
@@ -25,4 +67,17 @@ public class XMemcachedMainBench {
 	     System.out.println(value);
 	     System.exit(0);
 	}
+	
+	private static MemcachedClient createCluster() throws Throwable{
+		 XMemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses("127.0.0.1:11211"));
+	     MemcachedClient client = builder.build();
+	     client.setPrimitiveAsString(true);
+	     return client;
+	}
+	
+	private static void printBench(String msg,double init, double end){
+		int seconds = (int) ((end - init) / 1000) % 60 ;
+		System.out.println("TIME to " + msg + ": " + (end - init) + " ms - " + seconds + " s" );
+	}
+	
 }
