c8e7ae6b206029d20465fcf135b8456fb2013011
diegopacheco <diego.pacheco.it@gmail.com>
2015-10-16 15:52:15 -0300

    .
    .

--- EVcache-playground/src/main/java/com/github/diegopacheco/sandbox/java/memcached/bench/evcache/netflix/NetflixEVcacheMainBench.java
+++ EVcache-playground/src/main/java/com/github/diegopacheco/sandbox/java/memcached/bench/evcache/netflix/NetflixEVcacheMainBench.java
@@ -1,7 +1,38 @@
 package com.github.diegopacheco.sandbox.java.memcached.bench.evcache.netflix;
 
+import net.spy.memcached.CachedData;
+
+import com.netflix.evcache.EVCacheTranscoder;
+import com.netflix.evcache.pool.EVCacheClient;
+import com.netflix.evcache.pool.EVCacheClientPool;
+import com.netflix.evcache.pool.EVCacheClientPoolManager;
+
 public class NetflixEVcacheMainBench {
 	public static void main(String[] args) {
+		EVCacheClientPoolManager.getInstance().initEVCache("SimpleEvCacheBench");
+		EVCacheClientPoolManager pm = EVCacheClientPoolManager.getInstance();
+		
+		EVCacheClientPool pool = pm.getEVCacheClientPool("SimpleEvCacheBench");
+		EVCacheClient client = pool.getEVCacheClient();
+		
+		client.set("key1",new EVCacheTranscoder<String>() {
+			@Override
+			public boolean asyncDecode(CachedData arg0) {
+				return false;
+			}
+			@Override
+			public String decode(CachedData arg0) {
+				return null;
+			}
+			@Override
+			public CachedData encode(String arg0) {
+				return null;
+			}
+			@Override
+			public int getMaxSize() {
+				return 100;
+			}
+		});
 		
 	}
 }
