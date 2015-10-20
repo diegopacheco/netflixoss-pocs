6f0f7f82ed5b13333e66a0e989ea1fc190a3cc9f
diegopacheco <diego.pacheco.it@gmail.com>
2015-10-20 11:28:21 -0200

    .
    .

--- Dyno-NetflixDynomite-client-bench/src/main/java/com/github/diegopacheco/sandbox/java/jedis/bench/dyno/netflix/DynoJedisMainBench.java
+++ Dyno-NetflixDynomite-client-bench/src/main/java/com/github/diegopacheco/sandbox/java/jedis/bench/dyno/netflix/DynoJedisMainBench.java
@@ -22,62 +22,10 @@ public class DynoJedisMainBench {
 	private static Logger log = LoggerFactory.getLogger(DynoJedisMainBench.class);
 	
 	public static void main(String[] args) throws Throwable {
-		
-//		final HostSupplier customHostSupplier = new HostSupplier() {
-//			final List<Host> hosts = new ArrayList<Host>();
-//			   @Override
-//			   public Collection<Host> getHosts() {
-//			    hosts.add(new Host("127.0.0.1", 8102, Status.Up).setRack("us-west-2"));
-//			    return hosts;
-//			   }
-//		};
-//		DynoJedisClient dynoClient = new DynoJedisClient.Builder()
-//        		.withApplicationName("MY_APP")
-//        		.withDynomiteClusterName("us-west-2")
-//        		.withHostSupplier(customHostSupplier)
-//        		.build();
-//		
-//		 DynoJedisClient dynoClient = new DynoJedisClient.Builder()
-//        .withApplicationName("MY_APP")
-//        .withDynomiteClusterName("MY_APP_CLUSTER")
-//        .withCPConfig(new ArchaiusConnectionPoolConfiguration("MY_APP")
-//                      .setPort(8102)
-//                      .setMaxTimeoutWhenExhausted(1000)
-//                      .setMaxConnsPerHost(5)
-//        ).build();		
-		
-//		final int port = 8102; 
-//		final Host localHost = new Host("127.0.0.1", port, Status.Up);
-//		final HostSupplier localHostSupplier = new HostSupplier() {
-//			@Override
-//			public Collection<Host> getHosts() {
-//				return Collections.singletonList(localHost);
-//			}
-//		};
-//		
-//		DynoJedisClient dynoClient = new DynoJedisClient.Builder()
-//		.withApplicationName("app")
-//		.withDynomiteClusterName("app_cluster")
-//		.withHostSupplier(localHostSupplier)
-//		.withCPConfig(new ConnectionPoolConfigurationImpl("app").setLocalDC("us-west-2"))
-//		.withPort(port)
-//		.build();
 
-//		final String json = "[{\"token\":\"3303027599\",\"hostname\":\"127.0.0.1\",\"zone\":\"us-west-2\"}]\"";
-//		TokenMapSupplier testTokenMapSupplier = new AbstractTokenMapSupplier() {
-//		    @Override
-//		    public String getTopologyJsonPayload(String hostname) {
-//		        return json;
-//		    }
-//			@Override
-//			public String getTopologyJsonPayload(Set<Host> activeHosts) {
-//				return json;
-//			}
-//		};			
-		
-        System.setProperty("log4j.rootLogger", "ERROR");
-        BasicConfigurator.configure();
-        log.info("Logger intialized");
+//        System.setProperty("log4j.rootLogger", "ERROR");
+//        BasicConfigurator.configure();
+//        log.info("Logger intialized");
 		
 		final HostSupplier customHostSupplier = new HostSupplier() {
 			final List<Host> hosts = new ArrayList<Host>();
