01d5cf64bed7a9495da153bcdd7d306c83ed3f73
diegopacheco <diego.pacheco.it@gmail.com>
2015-12-28 13:11:09 -0200

    .
    .

--- scala-solr-sbt/src/main/scala/com/github/diegopacheco/sandbox/scala/solr/main/MainRunner.scala
+++ scala-solr-sbt/src/main/scala/com/github/diegopacheco/sandbox/scala/solr/main/MainRunner.scala
@@ -4,10 +4,10 @@ object MainRunner extends App {
     
     import jp.sf.amateras.solr.scala._
     
-    val client = new SolrClient("http://127.0.0.1:8983/solr/")
+    val client = new SolrClient("http://127.0.0.1:8983/solr/index.html#/maps2/")
     println("Client: " + client)
     
-    val result = client.query("/").getResultAsMap()
+    val result = client.query("*:*").getResultAsMap()
     println("Result: " + result)
   
 }
