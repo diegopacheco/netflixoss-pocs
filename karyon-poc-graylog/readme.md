7d7556bc590d9ca487dc6c75f3c12f1a4cc788df
Diego Pacheco <diego.pachecoit@gmail.com>
2016-04-21 19:06:22 -0300

    added to github

---
+++ karyon-poc-graylog/readme.md
@@ -0,0 +1,24 @@
+Build
+=====
+
+You need to bootup eureka server bofore run this otherwise it wont work out. 
+
+```bash
+./gradlew build
+```
+
+Run
+===
+
+```bash
+./gradlew run
+```
+
+Open the browser: http://127.0.0.1:8888/math/sum/1/1/
+
+Admin
+=====
+
+Go to: http://127.0.0.1:8077/admin
+
+
---
+++ karyon-poc-graylog/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/karyon/poc/jersey/MainRunner.java
@@ -0,0 +1,39 @@
+package com.github.diegopacheco.sandbox.java.netflixoss.karyon.poc.jersey;
+
+import java.net.InetAddress;
+
+import com.github.diegopacheco.sandbox.java.netflixoss.karyon.poc.rx.RxNettyHandler;
+
+import netflix.adminresources.resources.KaryonWebAdminModule;
+import netflix.karyon.Karyon;
+import netflix.karyon.KaryonBootstrapModule;
+import netflix.karyon.ShutdownModule;
+import netflix.karyon.archaius.ArchaiusBootstrapModule;
+import netflix.karyon.eureka.KaryonEurekaModule;
+import netflix.karyon.servo.KaryonServoModule;
+import netflix.karyon.transport.http.health.HealthCheckEndpoint;
+
+public class MainRunner {
+	public static void main(String[] args) throws Exception {
+		System.setProperty("java.awt.headless","true");
+		System.setProperty("archaius.deployment.environment","dev");
+		
+//      Graylog
+	
+		org.slf4j.MDC.put("ipAddress", InetAddress.getLocalHost().getHostAddress() );	
+		
+//      RxNetty		
+		
+		HealthcheckResource healthCheckHandler = new HealthcheckResource();
+        Karyon.forRequestHandler(8888, new RxNettyHandler("/healthcheck", new HealthCheckEndpoint(healthCheckHandler)),
+
+        		new KaryonBootstrapModule(healthCheckHandler),
+                new ArchaiusBootstrapModule("simplemath-netflix-oss"),
+                KaryonEurekaModule.asBootstrapModule(),
+                Karyon.toBootstrapModule(KaryonWebAdminModule.class),
+                ShutdownModule.asBootstrapModule(),
+                KaryonServoModule.asBootstrapModule()
+        ).startAndWaitTillShutdown();
+		
+	}
+}
