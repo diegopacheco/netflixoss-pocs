21fea7f2eda02a968d1448e507bb536e86344c1a
Diego Pacheco <diego.pachecoit@gmail.com>
2016-05-23 14:30:49 -0700

    rename and added a simple florida version

---
+++ dyomite-docker/florida/dynomite-with-florida/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+service redis-server start &
+
+mkdir /var/log/dynomite/
+
+sed -i 's/@rack/$DYNOMITE_RACK/g'     /dynomite/conf/dynomite_florida.yml
+sed -i 's/@tokens/$DYNOMITE_TOKENS/g' /dynomite/conf/dynomite_florida.yml
+
+/dynomite/src/dynomite --conf-file=/dynomite/conf/dynomite_florida.yml -v11 -M 200000 -o /var/log/dynomite/dynomite_log.txt
