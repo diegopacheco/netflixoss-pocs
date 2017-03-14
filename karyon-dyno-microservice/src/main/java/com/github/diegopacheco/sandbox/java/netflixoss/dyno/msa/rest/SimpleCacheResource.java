e8250f15e4226e0076e17796fa0831fc62943851
diegopacheco <diego.pacheco.it@gmail.com>
2017-03-13 18:28:10 -0700

    Added cross zone fallback

--- karyon-dyno-microservice/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/dyno/msa/rest/SimpleCacheResource.java
+++ karyon-dyno-microservice/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/dyno/msa/rest/SimpleCacheResource.java
@@ -45,7 +45,7 @@ public class SimpleCacheResource {
 				.withDynomiteClusterName("dynomiteCluster")
 				.withCPConfig(new ArchaiusConnectionPoolConfiguration("dynomiteCluster")
 						.withTokenSupplier(tms).setMaxConnsPerHost(1).setConnectTimeout(2000)
-						.setRetryPolicyFactory(new RetryNTimes.RetryFactory(3)))
+						.setRetryPolicyFactory(new RetryNTimes.RetryFactory(3,true)))
 				.withHostSupplier(hs).build();
 		
 		this.dyno = dynoClient;
