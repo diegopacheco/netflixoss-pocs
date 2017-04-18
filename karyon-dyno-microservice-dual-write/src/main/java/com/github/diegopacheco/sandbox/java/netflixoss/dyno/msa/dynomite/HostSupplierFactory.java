a20ee56d6f5cc3d9ca2aec53af21a495e353e3df
diegopacheco <diego.pacheco.it@gmail.com>
2017-04-18 10:11:43 -0700

    First commit on dual write poc

---
+++ karyon-dyno-microservice-dual-write/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/dyno/msa/dynomite/HostSupplierFactory.java
@@ -0,0 +1,35 @@
+package com.github.diegopacheco.sandbox.java.netflixoss.dyno.msa.dynomite;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.netflix.dyno.connectionpool.Host;
+import com.netflix.dyno.connectionpool.Host.Status;
+import com.netflix.dyno.connectionpool.HostSupplier;
+
+public class HostSupplierFactory {
+	
+	public static HostSupplier build(List<DynomiteNodeInfo> nodes){
+		final List<Host> hosts = new ArrayList<Host>();
+		
+		for(DynomiteNodeInfo node: nodes){
+			hosts.add(buildHost(node));
+		}
+		
+		final HostSupplier customHostSupplier = new HostSupplier() {
+		   @Override
+		   public Collection<Host> getHosts() {
+			   return hosts;
+		   }
+		};
+		return customHostSupplier;
+	}
+	
+	private static Host buildHost(DynomiteNodeInfo node){
+		Host host = new Host(node.getServer(),8102,node.getDc());
+		host.setStatus(Status.Up);
+		return host;
+	}
+	
+}
