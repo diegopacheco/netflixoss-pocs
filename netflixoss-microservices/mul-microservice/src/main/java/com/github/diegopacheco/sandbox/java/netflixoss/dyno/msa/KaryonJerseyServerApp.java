6e8e627df88d705faeb9c246a25b2f93b65f1b28
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-06-12 16:20:51 -0300

    Added wrappers and confs

---
+++ netflixoss-microservices/mul-microservice/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/dyno/msa/KaryonJerseyServerApp.java
@@ -0,0 +1,35 @@
+package com.github.diegopacheco.sandbox.java.netflixoss.dyno.msa;
+
+import com.github.diegopacheco.sandbox.java.netflixoss.dyno.msa.KaryonJerseyServerApp.KaryonJerseyModuleImpl;
+import com.github.diegopacheco.sandbox.java.netflixoss.dyno.msa.rest.DynoManager;
+import com.netflix.governator.annotations.Modules;
+
+import netflix.adminresources.resources.KaryonWebAdminModule;
+import netflix.karyon.KaryonBootstrap;
+import netflix.karyon.ShutdownModule;
+import netflix.karyon.archaius.ArchaiusBootstrap;
+import netflix.karyon.jersey.blocking.KaryonJerseyModule;
+import netflix.karyon.servo.KaryonServoModule;
+
+@ArchaiusBootstrap
+@KaryonBootstrap(name = "karyon-dyno-microservice", healthcheck = HealthcheckResource.class)
+@Modules(include = {
+        ShutdownModule.class,
+        KaryonWebAdminModule.class,
+        KaryonServoModule.class,
+        KaryonJerseyModuleImpl.class,
+        //KaryonEurekaModule.class 
+})
+public interface KaryonJerseyServerApp {
+	 class KaryonJerseyModuleImpl extends KaryonJerseyModule {
+	        @Override
+	        protected void configureServer() {
+	            bind(AuthenticationService.class).to(AuthenticationServiceImpl.class);
+	            bind(DynoManager.class).asEagerSingleton();
+	            
+	            interceptorSupport().forUri("/*").intercept(LoggingInterceptor.class);
+	            interceptorSupport().forUri("/cache").interceptIn(AuthInterceptor.class);
+	            server().port(6002).threadPoolSize(400);
+	        }
+	 }
+}
---
+++ netflixoss-microservices/mul-microservice/src/main/java/com/github/diegopacheco/sandbox/java/netflixoss/dyno/msa/rest/SimpleCacheResource.java
@@ -0,0 +1,45 @@
+package com.github.diegopacheco.sandbox.java.netflixoss.dyno.msa.rest;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@Path("/cache")
+public class SimpleCacheResource {
+
+	private static final Logger logger = LoggerFactory.getLogger(SimpleCacheResource.class);
+			
+	@GET
+	@Path("set/{k}/{v}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response set(@PathParam("k") String k,@PathParam("v") String v) {
+		try {
+			return Response.ok("200").build();
+		} catch (Exception e) {
+			logger.error("Error creating json response.", e);
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	@GET
+	@Path("get/{k}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get(@PathParam("k") String k) {
+		try {
+			return Response.ok( "200" ).build();
+		} catch (Exception e) {
+			logger.error("Error creating json response.", e);
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+
+}
